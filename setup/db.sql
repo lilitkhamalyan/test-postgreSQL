27ef1c614ae947dc5b07fffedf233848bfb93395
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-08 16:14:30 -0500

    Created new course table.

--- setup/db.sql
+++ setup/db.sql
@@ -6,8 +6,16 @@ create table student (
       student_name varchar (100) not null
     );
 -- insert data in Student table
-    insert into student (student_name)
-    values
+insert into student (student_name)
+      values
         ('James Smith'),
         ('Victoria Brown');
+-- create table course
+create table course (
+      course_id integer not null,
+      student_id integer references student(student_id),
+      course_name varchar(50),
+      teacher_name varchar (100),
+      primary key (course_id, student_id)
+    );
 
