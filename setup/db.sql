540a4c5cd5d5437dde48301d872affb6c3f0dd2b
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-08 15:12:55 -0500

    Fixed the comments and inserted data in the student table.

--- setup/db.sql
+++ setup/db.sql
@@ -1,13 +1,13 @@
-# create schema
+-- create schema
 create schema school;
-# create STUDENT table
+-- create STUDENT table
 create table student (
       student_id serial primary key,
       student_name varchar (100) not null
     );
-# insert data in Student table
+-- insert data in Student table
     insert into student (student_name)
     values
-        (James Smith),
-        (Victoria Brown);
+        ('James Smith'),
+        ('Victoria Brown');
 
