5209ad62ff960ad0a292ef9405f4106d2a7f2225
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-08 23:01:41 -0500

    Drop create statements file updated.

--- setup/db.sql
+++ setup/db.sql
@@ -1,21 +1,64 @@
 -- create schema
-create schema school;
--- create STUDENT table
-create table student (
-      student_id serial primary key,
-      student_name varchar (100) not null
-    );
--- insert data in Student table
-insert into student (student_name)
+drop schema if exists library cascade;
+create schema if not exists library;
+
+create extension if not exists "uuid-ossp" with schema library;
+
+-- create AUTHOR table
+drop table if exists library.authors;
+create table library.authors(
+      id uuid primary key,
+      first_name text not null,
+      last_name text not null,
+      pseudonyme text null
+    );  
+
+-- insert data in AUTHORS table
+insert into library.authors (id, first_name, last_name, pseudonyme)
       values
-        ('James Smith'),
-        ('Victoria Brown');
--- create table course
-create table course (
-      course_id integer not null,
-      student_id integer references student(student_id),
-      course_name varchar(50),
-      teacher_name varchar (100),
-      primary key (course_id, student_id)
+        (library.uuid_generate_v4(), 'James', 'Smith', 'Jimmy'),
+        (library.uuid_generate_v4(),'Victoria', 'Brown', null);
+
+-- create BOOKS table
+drop table if exists library.books;
+create table library.books (
+      id uuid not null,
+     author_id uuid references library.authors(id),
+      title text,
+      date_published text,
+     primary key (id)
     );
+
+-- insert data in BOOKS table
+insert into library.books (id, author_id,title, date_published)
+      values
+(library.uuid_generate_v4(),
+  (SELECT id FROM library.authors WHERE first_name = 'Victoria' AND last_name = 'Brown'),
+  'Flying Over Normandy',
+  'May 19, 1978'
+),
+(library.uuid_generate_v4(),
+  (SELECT id FROM library.authors WHERE first_name = 'James' AND last_name = 'Smith'),
+  'Postgres for Dummies',
+  'August 2, 1954'
+),
+(library.uuid_generate_v4(),
+  (SELECT id FROM library.authors WHERE first_name = 'James' AND last_name = 'Smith'),
+  'HTML 5 & CSS 3',
+  'April 4, 2013'
+),
+(library.uuid_generate_v4(),
+  (SELECT id FROM library.authors WHERE first_name = 'Victoria' AND last_name = 'Brown'),
+  'The Battle of Saratoga',
+  'September 24, 1972'
+),
+(library.uuid_generate_v4(),
+  (SELECT id FROM library.authors WHERE first_name = 'James' AND last_name = 'Smith'),
+  'Design Patterns',
+  'August 2, 1997'
+);
+
+
+
+
 
